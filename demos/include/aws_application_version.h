88695141fc1586bd49b38700bbd6837631175939
HirokiIshiguro <hiroki.ishiguro.fv@renesas.com>
2020-04-05 12:16:18 +0900

    [ADDED] benchmark appdata v101. [REMOVED] benchmark appdata v094.
    Signed-off-by: HirokiIshiguro <hiroki.ishiguro.fv@renesas.com>

--- demos/include/aws_application_version.h
+++ demos/include/aws_application_version.h
@@ -29,8 +29,8 @@
 #include "iot_appversion32.h"
 extern const AppVersion32_t xAppFirmwareVersion;
 
-#define APP_VERSION_MAJOR    0
-#define APP_VERSION_MINOR    9
-#define APP_VERSION_BUILD    4
+#define APP_VERSION_MAJOR    1
+#define APP_VERSION_MINOR    0
+#define APP_VERSION_BUILD    1
 
 #endif
